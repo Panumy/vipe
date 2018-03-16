120399bbc5e941e9e55151ec09e0b8f8e534bfe6
Toma <nerovani111@gmail.com>
2018-03-16 10:24:42 +0100

    initial commit

---
+++ VIPE.py
@@ -0,0 +1,312 @@
+import os
+
+def cls():
+    os.system(['clear','cls'][os.name == 'nt'])
+def name():
+    cls()
+    print "=====================================VIPE====================================="
+def house(adress, owner, number, picture):
+    print "House adress: {}".format(adress)
+    print "Owner: {}".format(owner)
+    print "Number: {}".format(number)
+    print "Picture: {}".format(picture)
+    l = raw_input("")
+
+#1: 6852 Mulholland
+sixeightfivetwo      = "TBA"
+sixeightfivetwoo     = "Dkkk"
+sixeightfivetwoa     = "6852 Mulholland"
+#2: 6910 Mulholland
+sixnineonezero       = "6085"
+sixnineonezeroo      = "James"
+sixnineonezeroa      = "6910 Mulholland"
+#3: 9778 East Los Santos
+ninesevenseveneight  = "BigD@SaMail.com ((Forum PM Scruffman))"
+ninesevenseveneighto = "BigD."
+ninesevenseveneighta = "9778 East Los Santos"
+#4: 4400 Mulholland
+fourfourzerozero     = "TBA"
+fourfourzerozeroo    = "HoneyBee"
+fourfourzerozeroa    = "4400 Mulholland"
+#5: 2338 Santa Maria Beach
+twothreethreeeight   = "TBA"
+twothreethreeeighto  = "Slick"
+twothreethreeeighta  = "2338 Santa Maria Beach"
+#6: 1309 Downtown Los Santos
+onethreezeronine     = "TBA"
+onethreezeronineo    = "TBA"
+onethreezeroninea    = "1309 Downtown Los Santos"
+#7: 9103 Verdant Bluffs
+nineonezerothree     = "TBA"
+nineonezerothreeo    = "TBA"
+nineonezerothreea    = "9103 Verdant Bluffs"
+#8: 6526 Mulholland
+sixfivetwosix        = "TBA"
+sixfivetwosixo       = "TBA"
+sixfivetwosixa       = "6526 Mulholland"
+#9: 4998 Marina
+fourninenineeight    = "TBA"
+fourninenineeighto   = "TBA"
+fourninenineeighta   = "4998 Marina"
+#10: 8481 Marina
+eightfoureightone    = "TBA"
+eightfoureightoneo   = "TBA"
+eightfoureightonea   = "8481 Marina"
+#11: 5352 Commerce
+fivethreefivetwo     = "TBA"
+fivethreefivetwoo    = "MitchyMitch"
+fivethreefivetwoa    = "5352 Commerce"
+#12: 8591 Idlewood
+eightfivenineone     = "91735"
+eightfivenineoneo    = "nope"
+eightfivenineonea    = "8591 Idlewood"
+#13: 7190 Mulholland
+sevenoneninezero     = "TBA"
+sevenoneninezeroo    = "TBA"
+sevenoneninezeroa    = "7190 Mulholland"
+#14: 8216 East Los Santos
+eighttwoonesix       = "TBA"
+eighttwoonesixo      = "TBA"
+eighttwoonesixa      = "8216 East Los Santos"
+#15: 6851 MulHolland
+sixeightfiveone      = "47152"
+sixeightfiveoneo     = "Yellow King"
+sixeightfiveonea     = "6851 Mulholland"
+#16: 6386 Marina
+sixthreeeightsix     = "TBA"
+sixthreeeightsixo    = "TBA"
+sixthreeeightsixa    = "6386 Marina"
+#17: 7290 Verona Beach
+seventwoninezero     = "ivan17528@gmail.com ((Forum PM Rhys))"
+seventwoninezeroo    = "Ivan14528"
+seventwoninezeroa    = "7290 Verona Beach"
+#18: 3631 East Beach
+threesixthreeone     = "444"
+threesixthreeoneo    = "Michael Greene"
+threesixthreeonea    = "3631 East Beach"
+#19: 1836 Marina
+oneeightthreesix     = "TBA"
+oneeightthreesixo    = "Speed"
+oneeightthreesixa    = "1836 Marina"
+#20: 3379 Beachview Apartments
+threethreesevennine  = "TBA"
+threethreesevennineo = "Kath"
+threethreesevenninea = "3379 Beachview Apartments"
+#21: 8746 Las Colinas
+eightsevenfoursix    = "TBA"
+eightsevenfoursixo   = "Peter"
+eightsevenfoursixa   = "8746 Las Colinas"
+#22: 8673 Rodeo
+eightsixseventhree   = "m.fisher@samail.com (( Forum PM Alan ))"
+eightsixseventhreeo  = "Fisher"
+eightsixseventhreea  = "8673 Rodeo"
+#23: 9616 East Los Santos
+ninesixonesix        = "TBA"
+ninesixonesixo       = "TBA"
+ninesixonesixa       = "9616 East Los Santos"
+#24: 1902 East Los Santos
+oneninezerotwo       = "TBA"
+oneninezerotwoo      = "TBA"
+oneninezerotwoa      = "1902 East Los Santos"
+#25: 8612 East Los Santos
+eightsixonetwo       = "TBA"
+eightsixonetwoo      = "TBA"
+eightsixonetwoa      = "8612 East Los Santos"
+#26: 1560 Rodeo apartment
+onefivesixzero       = "49"
+onefivesixzeroo      = "Tai Kaoru"
+onefivesixzeroa      = "1560 Rodeo apartment"
+#27: 8776 Richman
+eightsevensevensix   = "91891"
+eightsevensevensixo  = "Gregory"
+eightsevensevensixa  = "8776 Richman"
+#28: 3338 Mulholland
+threethreethreeeight = "TBA"
+threethreethreeeighto= "TBA"
+threethreethreeeighta= "3338 Mulholland"
+#29: 5721 Marina
+fiveseventwoone      = "TBA"
+fiveseventwooneo     = "Rose"
+fiveseventwoonea     = "5721 Marina"
+#30: 9976 Market
+nineninesevensix     = "TBA"
+nineninesevensixo    = "Cry"
+nineninesevensixa    = "9976 Market"
+#31: 8018 Market
+eightzerooneeight    = "TBA"
+eightzerooneeighto   = "TBA"
+eightzerooneeighta   = "8018 Market"
+#32: 2498 Ocean Docks
+twofournineeight     = "TBA"
+twofournineeighto    = "TBA"
+twofournineeighta    = "2498 Ocean Docks"
+#33: 2908 Idlewood
+twoninezeroeight     = "TBA"
+twoninezeroeighto    = "TBA"
+twoninezeroeighta    = "2908 Idlewood"
+#34: 7590 Market
+sevenfiveninezero    = "TBA"
+sevenfiveninezeroo   = "TBA"
+sevenfiveninezeroa   = "7590 Market"
+#35: 3298 Market
+threetwonineeight    = "40979"
+threetwonineeighto   = "Kazarian"
+threetwonineeighta   = "3298 Market"
+#36: 3258 Mulholland
+threetwofiveeight    = "TBA"
+threetwofiveeighto   = "Sky"
+threetwofiveeighta   = "3258 Mulholland"
+#37: 8753 Las Colinas
+eightsevenfivethree  = "26"
+eightsevenfivethreeo = "Macon"
+eightsevenfivethreea = "8753 Las Colinas"
+#38: 1752 Willowfield
+onesevenfivetwo      = "TBA"
+onesevenfivetwoo     = "TBA"
+onesevenfivetwoa     = "1752 Willowfield"
+#39: 1933 Marina
+oneseventhreethree   = "TBA"
+oneseventhreethreeo  = "TBA"
+oneseventhreethreea  = "1933 Marina"
+#40: 6068 Skyreach Apartment
+sixzerosixeight      = "TBA"
+sixzerosixeighto     = "Frankie"
+sixzerosixeighta     = "6068 Skyreach Apartment"
+#41: 1464 Jefferson
+onefoursixfour       = "45"
+onefoursixfouro      = "Antonio Caravello"
+onefoursixfoura      = "1464 Jefferson"
+#42: 8755 Idlewood
+eightsevenfivefive   = "TBA"
+eightsevenfivefiveo  = "TBA"
+eightsevenfivefivea  = "8755 Idlewood"
+#43: 7511 Jefferson
+sevenfiveoneone      = "TBA"
+sevenfiveoneoneo     = "TBA"
+sevenfiveoneonea     = "7511 Jefferson"
+#44: 1532 Rodeo
+onefivethreetwo      = "TBA"
+onefivethreetwoo     = "Cry"
+onefivethreetwoa     = "1532 Rodeo"
+#45: 2114 Mulholland
+twooneonefour        = "88"
+twooneonefouro       = "A. Howard"
+twooneonefoura       = "2114 Mulholland"
+#46: 8861 El Corona
+eighteightsixone     = "TBA"
+eighteightsixoneo    = "TBA"
+eighteightsixonea    = "8861 El Corona"
+
+houses = "46"
+
+while True:
+   name()
+   print '"VIPE" - Up to date as of 13:22 (26/08/2015)'
+   print "=============================================================================="
+   print "Number of houses in the database: {}".format(houses)
+   print "=============================================================================="
+   print "Changelog:"
+   print " - 6068 Skyreach Apartment has been added"
+   print " - 1464 Jefferson has been added"
+   print " - 8755 Idlewood has been added"
+   print " - 7511 Jefferson has been added"
+   print " - 1532 Rodeo has been added"
+   print " - 2114 Mulholland has been added"
+   print " - 8861 El Corona has been added"
+   l = raw_input("")
+   while True:
+       name()
+       ask = input("What house adress do you want to know info of?: ")
+       name()
+       print "===INFORMATION ABOUT HOUSE {}===".format(ask)
+       if (ask == 6852):
+          house(sixeightfivetwoa, sixeightfivetwoo, sixeightfivetwo, "Yes")
+       elif (ask == 6910):
+          house(sixnineonezeroa, sixnineonezeroo, sixnineonezero, "Yes")
+       elif (ask == 9778):
+          house(ninesevenseveneighta, ninesevenseveneighto, ninesevenseveneight, "Yes")
+       elif (ask == 4400):
+          house(fourfourzerozeroa, fourfourzerozeroo, fourfourzerozero, "Yes")
+       elif (ask == 2338):
+          house(twothreethreeeighta, twothreethreeeighto, twothreethreeeight, "Yes")
+       elif (ask == 1309):
+          house(onethreezeroninea, onethreezeronineo, onethreezeronine, "Yes")
+       elif (ask == 9103):
+          house(nineonezerothreea, nineonezerothreeo, nineonezerothree, "Yes")
+       elif (ask == 6526):
+          house(sixfivetwosixa, sixfivetwosixo, sixfivetwosix, "Yes")
+       elif (ask == 4998):
+          house(fourninenineeighta, fourninenineeighto, fourninenineeight, "Yes")
+       elif (ask == 8481):
+          house(eightfoureightonea, eightfoureightoneo, eightfoureightone, "Yes")
+       elif (ask == 5352):
+          house(fivethreefivetwoa, fivethreefivetwoo, fivethreefivetwo, "Yes")
+       elif (ask == 8591):
+          house(eightfivenineonea, eightfivenineoneo, eightfivenineone, "Yes")
+       elif (ask == 7190):
+          house(sevenoneninezeroa, sevenoneninezeroo, sevenoneninezero, "Yes")
+       elif (ask == 8216):
+          house(eighttwoonesixa, eighttwoonesixo, eighttwoonesix, "No")
+       elif (ask == 6386):
+          house(sixthreeeightsixa, sixthreeeightsixo, sixthreeeightsix, "No")
+       elif (ask == 7290):
+          house(seventwoninezeroa, seventwoninezeroo, seventwoninezero, "No")
+       elif (ask == 3631):
+          house(threesixthreeonea, threesixthreeoneo, threesixthreeone, "No")
+       elif (ask == 1836):
+          house(oneeightthreesixa, oneeightthreesixo, oneeightthreesix, "No")
+       elif (ask == 3379):
+          house(threethreesevenninea, threethreesevennineo, threethreesevennine, "No")
+       elif (ask == 8746):
+          house(eightsevenfoursixa, eightsevenfoursixo, eightsevenfoursix, "No")
+       elif (ask == 8673):
+          house(eightsixseventhreea, eightsixseventhreeo, eightsixseventhree, "No")
+       elif (ask == 9616):
+          house(ninesixonesixa, ninesixonesixo, ninesixonesix, "No")
+       elif (ask == 1902):
+          house(oneninezerotwoa, oneninezerotwoo, oneninezerotwo, "No")
+       elif (ask == 8612):
+          house(eightsixonetwoa, eightsixonetwoo, eightsixonetwo, "No")
+       elif (ask == 1560):
+          house(onefivesixzeroa, onefivesixzeroo, onefivesixzero, "No")
+       elif (ask == 8776):
+          house(eightsevensevensixa, eightsevensevensixo, eightsevensevensix, "No")
+       elif (ask == 3338):
+          house(threethreethreeeighta, threethreethreeeighto, threethreethreeeight, "No")
+       elif (ask == 5721):
+          house(fiveseventwoonea, fiveseventwooneo, fiveseventwoone , "No")
+       elif (ask == 9976):
+          house(nineninesevensixa, nineninesevensixo, nineninesevensix, "No")
+       elif (ask == 8018):
+          house(eightzerooneeighta, eightzerooneeighto, eightzerooneeight, "No")
+       elif (ask == 2498):
+          house(twofournineeighta, twofournineeighto, twofournineeight, "No")
+       elif (ask == 2908):
+          house(twoninezeroeighta, twoninezeroeighto, twoninezeroeight, "No")
+       elif (ask == 7590):
+          house(sevenfiveninezeroa, sevenfiveninezeroo, sevenfiveninezero, "No")
+       elif (ask == 3298):
+          house(threetwonineeighta, threetwonineeighto, threetwonineeight, "No")
+       elif (ask == 8753):
+          house(eightsevenfivethreea, eightsevenfivethreeo, eightsevenfivethree, "No")
+       elif (ask == 1752):
+          house(onesevenfivetwoa, onesevenfivetwoo, onesevenfivetwo, "No")
+       elif (ask == 1733):
+          house(oneseventhreethreea, oneseventhreethreeo, oneseventhreethree, "No")
+       elif (ask == 6068):
+          house(sixzerosixeighta, sixzerosixeighto, sixzerosixeight, "No")
+       elif (ask == 1464):
+          house(onefoursixfoura, onefoursixfouro, onefoursixfour, "No")
+       elif (ask == 8755):
+          house(eightsevenfivefivea, eightsevenfivefiveo, eightsevenfivefive, "No")
+       elif (ask == 7511):
+          house(sevenfiveoneonea, sevenfiveoneoneo, sevenfiveoneone, "No")
+       elif (ask == 1532):
+          house(onefivethreetwoa, onefivethreetwoo, onefivethreetwo, "No")
+       elif (ask == 2114):
+          house(twooneonefoura, twooneonefouro, twooneonefour, "No")
+       elif (ask == 8861):
+          house(eighteightsixonea, eighteightsixoneo, eighteightsixone, "No") 
+       else:
+          print "Adress {} was not found in the database.".format(ask)
+          l = raw_input("")
